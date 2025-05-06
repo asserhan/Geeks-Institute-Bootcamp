97f4192affd8d8eec8a81e5447df51ec210cb85b
asserhan <asseraouhanane@gmail.com>
2025-05-06 10:17:34 +0100

    finsih the first exo at gold xp

---
+++ week01/day01/Exercises_XP_Gold/Exercise1.py
@@ -0,0 +1,19 @@
+
+while 1:
+    try:
+        month=int(input("Invalid input. Please enter a number from 1 to 12 \n"))
+    except ValueError:
+        continue
+    except KeyboardInterrupt:
+        exit(1)
+    if (month < 1 or month > 12):
+        continue
+    if (int(month) >= 1 and int(month) <= 5):
+        print("The season is Spring")
+    elif (int(month) >= 6 and int(month) <= 8):
+        print("The season is Summer")
+    elif (int(month) >= 9 and int(month) <= 11):
+        print("The season is Autumn")
+    else:
+        print("The season is Winter")
+    break
