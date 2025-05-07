b24cf181984026ad5596b1310414b709822e7a74
asserhan <asseraouhanane@gmail.com>
2025-05-07 19:46:27 +0100

    finish challenge

--- week01/day02/Challenges/daily_challenge.py
+++ week01/day02/Challenges/daily_challenge.py
@@ -1,3 +1,6 @@
+dict_word={}
+
+
 while 1:
     has_digit=0
     word =input("Enter a word: ")
@@ -8,5 +11,11 @@ while 1:
         print("the word must be a string")
         continue
     else:
+         for item in enumerate(word):
+            if(item[1] not in dict_word):
+                 dict_word[item[1]] = []
+            dict_word[item[1]].append(item[0])
          break;
+
+print(dict_word)
             
