16def90c548fc7e31251743fa14beb14859fe3a4
asserhan <asseraouhanane@gmail.com>
2025-06-19 10:39:22 +0100

    display json data i react

---
+++ week07/day04/json-post-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HI this is a title</h1>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
